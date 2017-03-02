2c392895dcf6ed68ab24efd203fbc73c9577a033
samuelsson87 <samuelsson87@hotmail.com>
2017-03-02 14:03:31 +0100

    Fungerande kart-slider

--- js/fetch_data.js
+++ js/fetch_data.js
@@ -31,7 +31,7 @@ countries =
 var countries;
 var codeList;
 var test = [1,2,3];
-var year = 1960; 
+var year = 1960 ; 
 
 function readCountries(callback){
 	// Create list object containing objects with Key = Country Code and Value = {Country: Name of country} 
@@ -172,7 +172,7 @@ function readCo2(){
 function name2code(name){
 
 	if(codeList[name]==undefined){
-		//console.log(name + " stämmer ej med namn i land/kod-lista")
+		// console.log(name + " stämmer ej med namn i land/kod-lista")
 	}
 	return codeList[name];
 }
