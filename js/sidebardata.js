607c22faa44364efaf9422a1fc2c7fb09aaa17de
Fredrik Spansk <fredrik.spansk@gmail.com>
2017-03-08 16:36:48 +0100

    Top 5 working
    Almost. Need to sort first

--- js/sidebardata.js
+++ js/sidebardata.js
@@ -10,15 +10,33 @@ updateSideBar = function(){
 	d3.select("#co2-label").insert("h3").attr("class", "value").html(Math.round(countries[code].co2[year]*100)/100)
 	d3.select("#trade-label").insert("h3").attr("class", "value").html(Math.round(countries[code].tradingBalance[year]*100)/100)
 	d3.select('#additional-countries-hint').insert("p").attr("class", "value").html("Select two countries to compare them");
-	if (countries[code].exports[year] == undefined) {
-		console.log("No export data found for the year " + year);
+	if (countries[code].exports[year] == undefined || isEmpty(countries[code].exports[year])) {
 		d3.select('#top-export-container').insert("p").attr("class", "value").html("No export data for "+landETT.properties.name+" in "+year);
 	} else {
-		console.log("Export data not undefined" + countries[code].exports[year]["IND"].partner);
-		d3.select('#top-export-container').insert("h4").attr("class", "value").html("TOP 5 EXPORT");
+		d3.select('#top-5-export').insert("h4").attr("class", "value").html("TOP 5 EXPORT");
 		for (i in countries[code].exports[year]) {
-			d3.select('#top-5-export').insert("li").attr("class", "value").html(countries[code].exports[year][i].partner);
-			console.log("This is i: " + i);
+			d3.select('#top-5-export').insert("li").attr("class", "value").html(countries[code].exports[year][i].partner+
+			  " ("+countries[code].exports[year][i].mDollars+"m $)"
+			);
 		}
 	}
+	if (countries[code].imports[year] == undefined || isEmpty(countries[code].imports[year])) {
+		d3.select('#top-import-container').insert("p").attr("class", "value").html("No import data for "+landETT.properties.name+" in "+year);
+	} else {
+		d3.select('#top-5-import').insert("h4").attr("class", "value").html("TOP 5 IMPORT");
+		for (i in countries[code].imports[year]) {
+			d3.select('#top-5-import').insert("li").attr("class", "value").html(countries[code].imports[year][i].partner+
+			  " ("+countries[code].imports[year][i].mDollars+"m $)"
+			);
+		}
+	}
+}
+
+//Utility function to check if object is empty
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
 }
