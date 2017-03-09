41c0f006b6962b5483d2e5bc356390fb7b64a66a
josmol <josmol@kth.se>
2017-03-09 10:31:24 +0100

    size changes

--- js/draw_chart.js
+++ js/draw_chart.js
@@ -1,9 +1,33 @@
+
+var margin = {top:10, right:10, bottom:90, left:10};
+
+var width = 500 - margin.left - margin.right;
+
+var height = 1000 - margin.top - margin.bottom;
+
+var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .03)
+
+var yScale = d3.scale.linear()
+      .range([height, 0]);
+
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+      
+      
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+
+var barChartWidth = "80%";
 //Create svg-area for bar chart
 var svg = d3.select( "#chart")
             .append( "svg" )
             .attr('id', 'barChart')
-            .attr( "width", 1200 )
-            .attr( "height", 220 )
+            .attr( "width", width+margin.left + margin.right )
+            .attr( "height", height+margin.top + margin.bottom )
             .attr( "display", "block")
             .attr( "margin", "auto");
 
@@ -13,6 +37,12 @@ function drawBarChart(){
   console.log("in chart");
   //Year to display is now set in fetch_data.js
 
+  var data = d3.entries(countries).sort(
+                                        function(a,b){
+                                          //sorting from 1-8 of continentID to get 
+                                          //order of data for the bars to match the map
+                                          return d3.ascending(a.value.continentID, b.value.continentID);
+                                        });
   //Create tooltip
   var tip = d3.tip()
   .attr('class', 'd3-tip')
@@ -22,20 +52,20 @@ function drawBarChart(){
   })
 
 
+  xScale.domain(data.map(function(d) { return d.value.name; }));
+  yScale.domain([0, d3.max(data, function(d) { return d.value.co2[year]; })]);
+
 
   //Make selection and connect to data              
-  var selection = svg.selectAll( "rect" )
+  var selection = svg.selectAll( ".bar" )
                     //Fetching countries data and sorts it before creating bars
-                     .data(d3.entries(countries).sort(
-                                        function(a,b){
-                                          //sorting from 1-8 of continentID to get 
-                                          //order of data for the bars to match the map
-                                          return d3.ascending(a.value.continentID, b.value.continentID);
-                                        })
-                     );
+                     .data(data);
+
 
   svg.call(tip);
 
+  //LOOPA DETTA BEROENDE PÅ HUR MÅNGA SOM SKA VISAS 
+  
     //Create new bars
     selection.enter()
       .append( "rect" )
@@ -43,22 +73,16 @@ function drawBarChart(){
       .attr('id', function(d){
         return d.value.code
       })
-      .attr( "x", function(d,i){
-        return i*2;
-      })
-      .attr( "width", 1 )
+      .attr( "x", function(d) { return xScale(d.value.name); })
+      .attr( "width", xScale.rangeBand())
       .attr( "fill", "black" )
 
     //Set bar heights based on data
     selection
-      .attr( "height", function(d){
-        return d.value.co2[year]*5
-      })
+      .attr( "height", function(d) { return (height - yScale(d.value.co2[year]))/2; })
 
       //Set y position to get bars in right orientation
-      .attr( "y", function(d){
-        return 220 - d.value.co2[year]*5;
-      })
+      .attr( "y", function(d) { return yScale(d.value.co2[year])/2; })
 
       //Show tooltip on hover
       .on('mouseover', tip.show)
@@ -68,5 +92,42 @@ function drawBarChart(){
       selection.exit()
         .remove();
 
+}
 
+document.addEventListener("DOMContentLoaded", resizeBar);
+d3.select(window).on('resize', resizeBar); 
+
+function resizeBar() {
+  console.log('----resize function----');
+  // update width
+  width = parseInt(d3.select('#barChart').style('width'), 10);
+  width = width - margin.left - margin.right;
+
+  height = parseInt(d3.select("#barChart").style("height"));
+  height = height - margin.top - margin.bottom;
+  console.log('----resiz width----'+width);
+  console.log('----resiz height----'+height);
+  // resize the chart
+  
+    xScale.range([0, width]);
+    xScale.rangeRoundBands([0, width], .03);
+    yScale.range([height, 0]);
+    yScale.rangeRoundBands([0, height], .03);
+
+
+    d3.select(svgContainer.node().parentNode)
+        .style('width', (width + margin.left + margin.right) + 'px')
+        .style('height',((height + margin.top + margin.bottom) + "px"));
+
+    svgContainer.selectAll('.bar')
+      .attr("x", function(d) { return xScale(d.value.name); })
+      .attr("width", xScale.rangeBand())
+      .attr("y", function(d) { return yScale(d.value.co2[year]); })
+      .attr("height", yScale.rangeBand());
+               
+
+    // Swap the version below for the one above to disable rotating the titles
+    // svgContainer.select('.x.axis').call(xAxis.orient('top')).selectAll("text").attr("x",55).attr("y",-25);
+      
+   
 }
