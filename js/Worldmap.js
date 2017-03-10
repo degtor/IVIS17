95e9b4214c6581f63e31edcb14ffaa8f66c8ef98
VIKTOR <vikt.gustafsson@gmail.com>
2017-03-10 15:14:08 +0100

    Svart border på länder och samma tjocklek på zoom

--- js/Worldmap.js
+++ js/Worldmap.js
@@ -373,7 +373,7 @@ function move() {
   worldG.attr("transform", "translate(" + t + ")scale(" + s + ")");
 
   //adjust the country hover stroke width based on zoom level
-  d3.selectAll(".country").style("stroke-width", 1.5 / s);
+  //d3.selectAll(".country").style("stroke-width", 1.5 / s);
 
 }
 
