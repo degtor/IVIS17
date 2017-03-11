be2643fbe1001eaf3d68b09196e5187566c51d1b
josmol <josmol@kth.se>
2017-03-11 19:27:50 +0100

    legend - max 10 countries

--- js/linechart.js
+++ js/linechart.js
@@ -3,11 +3,16 @@ function drawLine(selectedCountries, id, type){
 
 //var data = d3.entries(countries[name2code(selectedCountries[0].properties.name)].co2);
 // var data2 = d3.entries(countries[name2code(selectedCountries[1].properties.name)].co2);
-
+var legendRectSize = 18;
+var legendSpacing = 4;
 // Set the dimensions of the canvas / graph
 var margin = {top: 30, right: 30, bottom: 30, left: 30},
     width = 450 - margin.left - margin.right,
     height = 320 - margin.top - margin.bottom;
+
+
+var radius = Math.min(width, height) / 2;
+
 
 // Set the ranges
 var x = d3.scale.linear().range([0, width]);
@@ -25,6 +30,10 @@ var yAxis = d3.svg.axis().scale(y)
 var valueline = d3.svg.line()
     .x(function(d) { return x(d.key); })
     .y(function(d) { return y(d.value); });
+ 
+ //Setting color scale
+    var colorList = ["#EEEEEE", "#0091EA"];
+    var lineColor = d3.scale.linear().domain([0,5]).range(colorList)
 
 
 // Adds the svg canvas
@@ -36,6 +45,43 @@ var mySVG = d3.select(id)
     .append("g")
         .attr("transform", 
               "translate(" + margin.left + "," + margin.top + ")");
+
+
+var legend = mySVG.selectAll('.legend')
+      .data(selectedCountries)
+      .enter()
+      .append('g')
+      .attr('class', 'line-legend')
+      .attr('transform', function(d, i) {
+    var height = -legendRectSize - legendSpacing;
+    var offset =  height * selectedCountries.length;
+    var horz = -2 * legendRectSize;
+    var vert = i * height - offset;
+    return 'translate(' + horz + ',' + vert + ')';
+  });
+
+
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+  .attr('height', legendRectSize)
+      .style('fill', function(d,i){ return lineColor(i);})
+      .style('stroke', function(d,i){ return lineColor(i);})
+      .attr('rx', 10)
+      .attr('ry', 10)
+      .attr("x", 450)
+
+
+
+    legend.append('text')
+      .attr('x', 470)
+      .attr("y", legendRectSize - legendSpacing)
+      .attr("class", function(d,i){return "textis-"+name2code(selectedCountries[i].properties.name)})
+
+      .text(function(d){ return d.properties.name;})
+        .attr('text-transform', 'capitalize')
+        .attr('font-size', '12px')
+        .attr('opacity', 0.5)
 
 
 // Get the data
@@ -62,10 +108,8 @@ var mySVG = d3.select(id)
 
     //#FF5252",
 
-    //Setting color scale
-    var colorList = ["#EEEEEE", "#0091EA"];
-    var lineColor = d3.scale.linear().domain([0,5]).range(colorList)
-
+    
+  
     // Add the valueline path.
     mySVG.append("path")
         .attr("class", "line")
@@ -75,11 +119,13 @@ var mySVG = d3.select(id)
         .on("mouseover", function (d) {    
           d3.select(this)                          //on mouseover of each line, give it a nice thick stroke
           .style("stroke-width",'4px');
-          
+          d3.selectAll(".textis-"+this.id).style("opacity", 1)
+
         })
         .on("mouseout", function(d) {        //undo everything on the mouseout
             d3.select(this)
-              .style("stroke-width",'2px');      
+              .style("stroke-width",'2px'); 
+              d3.selectAll(".textis-"+this.id).style("opacity", 0.5)     
         })
 
         //then append some 'nearly' invisible circles at each data point  
@@ -157,6 +203,7 @@ else{
       .text("YEAR"); 
 
 }
+
 
 function clearLineChart(id){
     var mySVG = d3.select(id).html("");
